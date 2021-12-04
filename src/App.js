ec023764d8d4dbce67d319e49852b86f124a07d2
Willigue <wiliiamher@yahoo.com>
2021-12-04 15:02:56 -0500

    Dinamic list, delete, styles & selection

--- src/App.js
+++ src/App.js
@@ -25,21 +25,35 @@ export function App(){
         localStorage.setItem(KEY, JSON.stringify(contacts))
     }, [contacts])
 
+    const checkContact = (id) => {
+        const newContacts = [...contacts]
+        const contact = newContacts.find((contact) => contact.id === id)
+        contact.isSelected = !contact.isSelected
+        setContacts(newContacts)
+    }
+
     const addContact = () => {
         const name = contactRef.current.value
         if(name=== '') return
         setContacts((oldContacts)=>{
-            return [...oldContacts, {id: uuid(), name}]
+            return [...oldContacts, {id: uuid(), name, isSelected: false}]
         })
 
         contactRef.current.value = null
     }
 
+    const deleteContact = () =>{
+        const selectedContacts = contacts.filter((contact) => !contact.isSelected)
+        setContacts(selectedContacts)
+    }
+
     return (
         <Fragment>
-        <ContactList contacts = {contacts} />
+        <ContactList contacts={contacts} checkContact={checkContact} />
         <input ref={contactRef} type="text" placeholder="nuevo contacto"/>
-        <button onClick={addContact}>😀Add</button>
+        <button onClick={addContact}>🙏Add</button>
+        <button onClick={deleteContact} >🙅</button>
+        <p> 🙋 {contacts.filter((contact) => contact.isSelected).length} contactos seleccionados </p>
         </Fragment>
     // const contacts = [
         
