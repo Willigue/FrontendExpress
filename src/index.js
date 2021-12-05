f79025c89c466b1d581091616f9830274ba143be
Willigue <wiliiamher@yahoo.com>
2021-12-05 01:00:29 -0500

    environment variable, router, login & connect backend

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import React from "react";
 import ReactDom from "react-dom";
-import { App } from "./App";
+import {BrowserRouter} from "react-router-dom"
+//import { App } from "./App";
+import Router from './Router'
+require('dotenv').config()
 
-ReactDom.render( <App /> , document.getElementById('root'))
+// ReactDom.render( <App /> , document.getElementById('root'))
+ReactDom.render(
+    <BrowserRouter>
+        <Router />
+    </BrowserRouter> 
+    , document.getElementById('root'))
